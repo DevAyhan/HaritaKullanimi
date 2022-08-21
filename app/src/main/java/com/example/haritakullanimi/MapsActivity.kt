bcd40a57c8765fd77cdf104474183685b355fe3a
DevAyhan <105974668+DevAyhan@users.noreply.github.com>
2022-08-21 14:50:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/haritakullanimi/MapsActivity.kt
@@ -0,0 +1,49 @@
+package com.example.haritakullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.haritakullanimi.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        binding.buttonKonumaGit.setOnClickListener {
+            //41.0296714,29.0386673,12z
+            val konum = LatLng(41.0296714,29.0386673)
+            mMap.addMarker(MarkerOptions().position(konum).title("Üsküdar").icon(BitmapDescriptorFactory.fromResource(R.drawable.konum_resim)))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(konum,12f))
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
